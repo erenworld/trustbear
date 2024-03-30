f2e586ff70d65204b8c6ff099b3702b03a9b409b
Eren Turkoglu <144266052+erenworld@users.noreply.github.com>
2024-03-30 17:48:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+    (function(w){
+        var k="nudgify",n=w[k]||(w[k]={});
+        n.uuid="b4750ccd-3ca4-40cd-abc2-bc7fece76b82";
+        var d=document,s=d.createElement("script");
+        s.src="https://pixel.nudgify.com/pixel.js";
+        s.async=1;
+        s.charset="utf-8";
+        d.getElementsByTagName("head")[0].appendChild(s)
+    })(window)
